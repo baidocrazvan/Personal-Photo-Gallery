1c84c92aa0f0aaea5ee0980cfb128d983a630e2c
Baidoc Razvan <baidocrazvan@gmail.com>
2025-03-07 13:39:41 +0200

    Add text to each image and render it visible only when it is facing the user

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let rotationAngle = 0;
 const numberOfImages = 8; // Number of images inside slider
 const angleBetweenImages = 360 / numberOfImages; // Angle between each image, in degrees
 
+updateVisibleText();
+
 function animateToLeft() {
     const targetAngle = rotationAngle + angleBetweenImages;
 
@@ -22,6 +24,7 @@ function animateToLeft() {
 
         rotationAngle += 0.5; // Increment the rotation angle forwards (adjust speed here)
         slider.style.transform = `perspective(1000px)  rotateY(${rotationAngle}deg)`;
+        updateVisibleText();
         animationFrameId = requestAnimationFrame(animate);
     }
 
@@ -43,11 +46,27 @@ function animateToRight() {
 
         rotationAngle -= 0.5; // Increment the rotation angle backwards (adjust speed here)
         slider.style.transform = `perspective(1000px)  rotateY(${rotationAngle}deg)`;
+        updateVisibleText();
         animationFrameId = requestAnimationFrame(animate);
     }
 
     animate(); // Start animation
 
+}
+
+function updateVisibleText() { // make <p> items visible only when image is facing the user
+    const items = document.querySelectorAll('.slider .item');
+    items.forEach((item, index) => {
+        // Calculate the angle of each item
+        const itemAngle = (index * angleBetweenImages + rotationAngle) % 360 // modulo '%' ensures value stays between 0 and 360
+
+        // Add visible class if the item is within 22.5 degrees away from 0 or 360 degrees, otherwise hide it.
+        if (Math.abs(itemAngle) < angleBetweenImages / 2 || Math.abs(itemAngle - 360) < angleBetweenImages / 2) {
+            item.classList.add('visible');
+        } else {
+            item.classList.remove('visible');
+        }
+    });
 }
 
 toggleLeft.addEventListener('click', () => {
